ab63168b85331f6ddc7e437e7a4f61fa8c499e5e
Tom Ierino <19969307@student.curtin.edu.au>
2020-08-14 22:42:59 +0800

    local auth

---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import { reduxForm, Field } from "redux-form";
+import { compose } from "redux";
+import { connect } from "react-redux";
+
+import * as actions from "../../actions";
+
+function Signup(props) {
+  // If user is signed in, redirect them to the homepage
+  useEffect(() => {
+    if (props.user) {
+      props.history.push("/home");
+    }
+  }, [props.history, props.user]);
+
+  function onSubmit(formProps) {
+    props.signup(formProps, () => {
+      props.history.push("/home");
+    });
+  }
+
+  return (
+    <form onSubmit={props.handleSubmit(onSubmit)}>
+      <fieldset>
+        <label>Email</label>
+        <Field name="email" type="text" component="input" autoComplete="off" />
+      </fieldset>
+      <fieldset>
+        <label>Password</label>
+        <Field
+          name="password"
+          type="password"
+          component="input"
+          autoComplete="off"
+        />
+      </fieldset>
+      <div>{props.error}</div>
+      <button>Sign up</button>
+    </form>
+  );
+}
+
+function mapStateToProps(state) {
+  return { error: state.auth.error };
+}
+
+export default compose(
+  connect(mapStateToProps, actions),
+  reduxForm({ form: "signup" })
+)(Signup);
