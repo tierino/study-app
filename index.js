523e028044b51af7dfe96989f54688052157bb83
Tom Ierino <19969307@student.curtin.edu.au>
2020-09-01 14:13:29 +0800

    heroku test

--- index.js
+++ index.js
@@ -35,14 +35,14 @@ require("./routes/authRoutes")(app);
 require("./routes/unitRoutes")(app);
 require("./routes/accountRoutes")(app);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+//if (process.env.NODE_ENV === "production") {
+app.use(express.static("client/build"));
 
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+//}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
